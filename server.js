15e7d6d2404b15c6bd60d80b34dd62877ebb6e50
Mikito Takada <mikito.takada@gmail.com>
2012-06-25 19:15:17 -0700

    Fix server.js

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var http = require('http'),
-    app = require('./lib/app.js'),
-    config = require('./config.js'),
-    api = require('./lib/api.js');
 
-app.configure(config);
+    api = require('./lib/api.js'),
+    Cache = require('./lib/cache.js'),
+    Package = require('./lib/package.js'),
+
+    config = require('./config.js');
+
+Cache.configure({
+    cacheDirectory: config.cacheDirectory,
+    cacheAge: config.cacheAge
+  });
+Package.configure({
+  cache: Cache,
+  externalUrl: config.externalUrl
+});
 
 var server = http.createServer();
 
@@ -13,3 +23,5 @@ server.on('request', function(req, res) {
     res.end();
   }
 }).listen(config.port, config.host);
+
+console.log('npm_lazy at', config.host, 'port', config.port);
