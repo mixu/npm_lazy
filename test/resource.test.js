ea29e773891aa2a0c9b07f998939737d9c2b8ca3
Mikito Takada <mikito.takada@gmail.com>
2013-11-05 18:33:23 -0800

    Move to file based fixtures so that the interface can use readableStreams

--- test/resource.test.js
+++ test/resource.test.js
@@ -1,53 +1,53 @@
-var assert = require('assert'),
+var fs = require('fs'),
+    assert = require('assert'),
     npmLazy = require('npm_lazy'),
-    Resource = npmLazy.Resource;
-
-var datasets = {
-  'local-cached': JSON.stringify({ name: 'local-cached' }),
-  'remote-valid': JSON.stringify({ name: 'remote-valid' }),
-  'remote-retry': 'aaa',
-  'remote-invalid': 'aaa',
-  'local-outdated': JSON.stringify({ name: 'uptodate' }),
-  'local-outdated-fail': 'aaa',
-  'remote-cached.tgz': 'remote-cached-tar',
-  'remote-valid.tgz': 'remote-valid-tar',
-};
-
-var local = [ 'local-cached', 'remote-cached.tgz' ], cache = {};
-
-local.forEach(function(key) {
-  cache[key] = datasets[key];
-});
-
-// remote has newer version of this
-cache['local-outdated'] = JSON.stringify({ name: 'outdated' });
-local.push('local-outdated');
-cache['local-outdated-fail'] = JSON.stringify({ name: 'outdated-fail' });
-local.push('local-outdated-fail');
+    Resource = npmLazy.Resource,
+    Cache = require('../lib/cache2.js');
 
 exports['resource tests'] = {
 
   before: function() {
+
+    // fixture setup
+    var cache = new Cache({ path: __dirname + '/tmp' }),
+        remoteDir = __dirname + '/fixtures/remote',
+        localDir = __dirname + '/fixtures/local';
+    Resource.setCache(cache);
+
+    // for each file in fixtures/local, store them in the cache
+    // as if they had already been downloaded
+
+    fs.readdirSync(localDir).forEach(function(basename) {
+      var filename = localDir + '/' + basename,
+          cachename = cache.filename(),
+          content = fs.readFileSync(filename);
+
+      console.log(filename, content.toString());
+
+      fs.writeFileSync(cachename, content);
+
+      cache.complete(basename, 'GET', cachename);
+    });
+
     // change the _fetchTask
     Resource.prototype._fetchTask = function(onDone) {
       console.log('fetch:', this.url);
-      // special case remote-retry: should succeed on 3rd try
-      if(this.url == 'remote-retry' && this.retries == 3) {
-        datasets[this.url] = JSON.stringify( { name: 'remote-retry' });
+
+      if(!fs.existsSync(remoteDir + '/' + this.url)) {
+        throw new Error('Path does not exist ' + remoteDir + '/' + this.url);
+        return;
       }
-      onDone(null, datasets[this.url]);
-    };
-    Resource.prototype.exists = function() {
-      return local.indexOf(this.url) != -1;
-    };
-    Resource.prototype._readCache = function(onDone) {
-      console.log('read-cache:', this.url);
-      return cache[this.url];
-    };
-    Resource.prototype._writeCache = function(data, onDone) {
-      console.log('write-cache:', this.url);
-      cache[this.url] = data;
-      onDone();
+
+      // special case remote-retry: should succeed on 3rd try
+      if(this.url == 'remote-retry.json' && this.retries == 3) {
+        // return remote-retry-valid.json
+        return onDone(null, fs.createReadStream(remoteDir + '/remote-retry-valid.json'));
+      }
+      // this works by reading the corresponding file from fixtures/remote
+
+      console.log(remoteDir + '/' + this.url);
+
+      return onDone(null, fs.createReadStream(remoteDir + '/' + this.url));
     };
   },
 
@@ -63,7 +63,7 @@ exports['resource tests'] = {
   'index resource': {
 
     'if it exists and is up to date, success': function(done) {
-      var r = Resource.get('local-cached');
+      var r = Resource.get('local-cached.json');
 
       r.getReadableStream(function(err, data) {
         assert.ok(!err);
@@ -73,7 +73,7 @@ exports['resource tests'] = {
     },
 
     'if it does not exist and the response is a JSON object, success': function(done) {
-      var r = Resource.get('remote-valid');
+      var r = Resource.get('remote-valid.json');
 
       r.getReadableStream(function(err, data) {
         assert.ok(!err);
@@ -83,7 +83,7 @@ exports['resource tests'] = {
     },
 
     'if it does not exist and the response is not a JSON object, retry': function(done) {
-      var r = Resource.get('remote-retry');
+      var r = Resource.get('remote-retry.json');
 
       r.getReadableStream(function(err, data) {
         assert.ok(!err);
@@ -93,7 +93,7 @@ exports['resource tests'] = {
     },
 
     'if retries > maxRetries, throw a error': function(done) {
-      var r = Resource.get('remote-invalid');
+      var r = Resource.get('remote-invalid.json');
 
       r.getReadableStream(function(err, data) {
         assert.ok(err);
@@ -103,7 +103,7 @@ exports['resource tests'] = {
     },
 
     'if the resource exists but is outdated, fetch a new version and return it': function(done) {
-      var r = Resource.get('local-outdated');
+      var r = Resource.get('local-outdated.json');
 
       r.isUpToDate = function() { return false; };
       r.getReadableStream(function(err, data) {
@@ -114,7 +114,7 @@ exports['resource tests'] = {
     },
 
     'if the resource is outdated and the fetch fails, return the cached version': function(done) {
-      var r = Resource.get('local-outdated-fail');
+      var r = Resource.get('local-outdated-fail.json');
 
       r.isUpToDate = function() { return false; };
       r.getReadableStream(function(err, data) {
@@ -141,7 +141,7 @@ exports['resource tests'] = {
     }
 
   },
-
+/*
   'tar resource': {
 
     'if it exists, success': function(done) {
@@ -187,7 +187,7 @@ exports['resource tests'] = {
     }
 
   }
-
+*/
 };
 
 // if this module is the script being run, then run the tests:
