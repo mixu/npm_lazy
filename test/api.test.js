ae660124d6e9f3e267bad830e498289151363fb3
Mikito Takada <mikito.takada@gmail.com>
2013-11-18 19:31:44 -0800

    Make the tests complain if mocha is not installed

--- test/api.test.js
+++ test/api.test.js
@@ -95,6 +95,14 @@ exports['given a server'] = {
 // if this module is the script being run, then run the tests:
 if (module == require.main) {
   var mocha = require('child_process').spawn('mocha', [ '--colors', '--ui', 'exports', '--reporter', 'spec', __filename ]);
+  mocha.on('error', function() {
+     console.log('Failed to start child process. You need mocha: `npm install -g mocha`');
+  });
+  mocha.stderr.on('data', function (data) {
+    if (/^execvp\(\)/.test(data)) {
+     console.log('Failed to start child process. You need mocha: `npm install -g mocha`');
+    }
+  });
   mocha.stdout.pipe(process.stdout);
   mocha.stderr.pipe(process.stderr);
 }
