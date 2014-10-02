ff80e5ef1ea2af1d77538ffb78f736b29993d546
Chris Close <christopher.close@gmail.com>
2014-10-02 22:55:58 +1000

    Change the signature for guard release to include errors
    So we can return upstream errors when requests are blocking

--- test/lifecycle.test.js
+++ test/lifecycle.test.js
@@ -16,7 +16,7 @@ describe('given a lifecycle', function() {
   it('can block and release', function(done) {
     this.cycle.block('foo');
     assert.ok(this.cycle.isBlocking('foo'));
-    this.cycle.release('foo');
+    this.cycle.release(null, 'foo');
     assert.ok(!this.cycle.isBlocking('foo'));
     done();
   });
@@ -27,7 +27,7 @@ describe('given a lifecycle', function() {
     this.cycle.onRelease('foo', function() { assertions++; if (assertions == 3) { done(); } });
     this.cycle.onRelease('foo', function() { assertions++; if (assertions == 3) { done(); } });
     this.cycle.onRelease('foo', function() { assertions++; if (assertions == 3) { done(); } });
-    this.cycle.release('foo');
+    this.cycle.release(null, 'foo');
   });
 
 });
